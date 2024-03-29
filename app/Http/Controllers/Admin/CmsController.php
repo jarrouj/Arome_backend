a757e03ff96f7439bdb373f80ce29d5642767a9b
= <=>
2024-03-29 21:55:26 +0200

    Small fixes in Dashboard

--- app/Http/Controllers/Admin/CmsController.php
+++ app/Http/Controllers/Admin/CmsController.php
@@ -7,6 +7,11 @@ use App\Models\Date;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\Offer;
+use App\Models\Order;
+use App\Models\Product;
+use App\Models\Subscriber;
+use App\Models\Transaction;
 use Illuminate\Support\Facades\Session;
 
 
@@ -14,8 +19,50 @@ class CmsController extends Controller
 {
     public function dash()
     {
-        $user =  User::latest()->paginate(5);
-        return view('admin.home', compact('user'));
+        $user                       =  User::latest()->paginate(5); // all users
+        $NumberOfUsers              =  User::where('usertype' , 0)->count(); // Customers non admin users
+        $NumberOfOrdersConfirmed    = Order::where('confirm' , 1)->count(); //Total Number of Confirmed Orders
+        $NumberOfOrdersNonConfirmed = Order::where('confirm' , null)->count(); //Total Number of Confirmed Orders
+        $NumberOfActiveOffers       = Offer::where('active' , 1)->count(); //Total Number of Active Offers
+        $NumberOfProducts           = Product::all()->count(); // Total number of products
+        $NumberOfSubscribers        = Subscriber::all()->count(); // Total subscribers
+
+        $Dates = session()->get('selected_date_range', []);
+
+        $startDate = $Dates[0] ?? null;
+        $endDate   = $Dates[1] ?? null;
+
+        //Revenue
+        $Revenue = 0;
+
+        if ($startDate && $endDate) {
+            $order = Order::where('confirm' , 1)->where('method' , 1)->whereBetween('created_at' , [$startDate , $endDate])->get();
+
+            foreach($order as $orders)
+            {
+                $Revenue += $orders->total_usd;
+            }
+        }else
+        {
+            $order = Order::where('confirm' , 1)->where('method' , 1)->get();
+
+            foreach($order as $orders)
+            {
+                $Revenue += $orders->total_usd;
+            }
+        }
+
+
+
+        return view('admin.home', compact('user' ,
+                                          'NumberOfUsers' ,
+                                          'NumberOfOrdersConfirmed' ,
+                                          'Revenue' ,
+                                          'NumberOfOrdersNonConfirmed' ,
+                                          'NumberOfActiveOffers' ,
+                                          'NumberOfProducts' ,
+                                          'NumberOfSubscribers'
+                                        ));
     }
 
 
--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -48,16 +48,110 @@
                             <div class="row">
                                 <div class="col-8">
                                     <div class="numbers">
-                                        <p class="text-sm mb-0 text-uppercase font-weight-bold">Number of Vehicles</p>
+                                        <p class="text-sm mb-0 text-uppercase font-weight-bold">Number of Users</p>
                                         <h5 class="font-weight-bolder">
-                                            5
+                                            {{ $NumberOfUsers }}
+                                        </h5>
+                                    </div>
+                                </div>
+                                <div class="col-4 text-end">
+
+                                    <div
+                                    class="icon icon-shape bg-gradient-success shadow-success text-center rounded-circle">
+                                    <i class="bi bi-people-fill text-lg opacity-10" aria-hidden="true"></i>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+                    <div class="card">
+                        <div class="card-body p-3">
+                            <div class="row">
+                                <div class="col-8">
+                                    <div class="numbers">
+                                        <p class="text-sm mb-0 text-uppercase font-weight-bold">Number of Orders Confirmed</p>
+                                        <h5 class="font-weight-bolder">
+                                            {{ $NumberOfOrdersConfirmed }}
+                                        </h5>
+                                    </div>
+                                </div>
+                                <div class="col-4 text-end">
+                                     <div
+                                        class="icon icon-shape bg-gradient-primary shadow-primary text-center rounded-circle">
+                                        <i class="bi bi-box-seam-fill text-lg opacity-10" aria-hidden="true"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-sm-6">
+                    <div class="card">
+                        <div class="card-body p-3">
+                            <div class="row">
+                                <div class="col-8">
+                                    <div class="numbers">
+                                        <p class="text-sm mb-0 text-uppercase font-weight-bold">New Orders
+                                        </p>
+                                        <h5 class="font-weight-bolder">
+                                            {{ $NumberOfOrdersNonConfirmed }}
                                         </h5>
                                     </div>
                                 </div>
                                 <div class="col-4 text-end">
                                     <div
-                                        class="icon icon-shape bg-gradient-primary shadow-primary text-center rounded-circle">
-                                        <i class="bi bi-car-front text-lg opacity-10" aria-hidden="true"></i>
+                                        class="icon icon-shape bg-gradient-warning shadow-warning text-center rounded-circle">
+                                        <i class="bi bi-box-seam-fill text-lg opacity-10" aria-hidden="true"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row mt-2">
+                <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+                    <div class="card">
+                        <div class="card-body p-3">
+                            <div class="row">
+                                <div class="col-8">
+                                    <div class="numbers">
+                                        <p class="text-sm mb-0 text-uppercase font-weight-bold">Revenue</p>
+                                        <h5 class="font-weight-bolder">
+                                            {{ $Revenue }}
+                                        </h5>
+                                    </div>
+                                </div>
+                                <div class="col-4 text-end">
+                                    <div
+                                    class="icon icon-shape bg-gradient-danger shadow-danger text-center rounded-circle">
+                                    <i class="bi bi-cash-coin text-lg opacity-10" aria-hidden="true"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+                    <div class="card">
+                        <div class="card-body p-3">
+                            <div class="row">
+                                <div class="col-8">
+                                    <div class="numbers">
+                                        <p class="text-sm mb-0 text-uppercase font-weight-bold">Number of Active Offers</p>
+                                        <h5 class="font-weight-bolder">
+                                            {{ $NumberOfActiveOffers }}
+                                        </h5>
+                                    </div>
+                                </div>
+                                <div class="col-4 text-end">
+                                    <div
+                                        class="icon icon-shape bg-gradient-success shadow-success text-center rounded-circle">
+                                        <i class="bi bi-percent text-lg opacity-10" aria-hidden="true"></i>
                                     </div>
                                 </div>
                             </div>
@@ -71,16 +165,16 @@
                             <div class="row">
                                 <div class="col-8">
                                     <div class="numbers">
-                                        <p class="text-sm mb-0 text-uppercase font-weight-bold">Number of Users</p>
+                                        <p class="text-sm mb-0 text-uppercase font-weight-bold">Number of Products</p>
                                         <h5 class="font-weight-bolder">
-                                            admin
+                                            {{ $NumberOfProducts }}
                                         </h5>
                                     </div>
                                 </div>
                                 <div class="col-4 text-end">
                                     <div
-                                        class="icon icon-shape bg-gradient-success shadow-success text-center rounded-circle">
-                                        <i class="bi bi-people-fill text-lg opacity-10" aria-hidden="true"></i>
+                                        class="icon icon-shape bg-gradient-primary shadow-primary text-center rounded-circle">
+                                        <i class="bi bi-boxes text-lg opacity-10" aria-hidden="true"></i>
                                     </div>
                                 </div>
                             </div>
@@ -96,7 +190,7 @@
                                         <p class="text-sm mb-0 text-uppercase font-weight-bold">Total Subscribers
                                         </p>
                                         <h5 class="font-weight-bolder">
-                                            3
+                                            {{ $NumberOfSubscribers }}
                                         </h5>
                                     </div>
                                 </div>
@@ -112,7 +206,8 @@
                 </div>
             </div>
 
-            <div class="row">
+
+            {{-- <div class="row">
                 <div class="col-12 my-4">
                     <div class="card">
                         <div class="card-body p-3">
@@ -138,7 +233,7 @@
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> --}}
 
             <div class="row mt-4">
                 <div class="col-lg-12 mb-lg-0 mb-4">
